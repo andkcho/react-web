bab7d710dffaaa9a48d61dd17efe7def4a9fcfac
andkcho <andrewc801@gmail.com>
2021-02-02 22:46:54 -0700

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,26 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button } from './Button';
 
 function Navbar () {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true)
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+
+    window.addEventListener('resize', showButton);
+
     return(
         <>
           <nav className='navbar'>
@@ -23,11 +38,22 @@ function Navbar () {
                         </Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
                             Services
                         </Link>
                     </li>
+                    <li className='nav-item'>
+                        <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                            Products
+                        </Link>
+                        <li className='nav-item'>
+                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            Sign up
+                        </Link>
+                    </li>
+                    </li>
                 </ul>
+                {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
             </div>
           </nav>
         </>
